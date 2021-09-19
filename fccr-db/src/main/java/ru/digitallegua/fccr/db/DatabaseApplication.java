3796728b09237401e0653ef41d449ad39427dbf2
rufabgoo <rufabgoo@gmail.com>
2021-09-19 23:58:34 +0200

    feature(db) - start configuration

---
+++ fccr-db/src/main/java/ru/digitallegua/fccr/db/DatabaseApplication.java
@@ -0,0 +1,46 @@
+package ru.digitallegua.fccr.db;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class DatabaseApplication {
+
+    @Autowired
+    private Environment env;
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DatabaseApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(String.format("jdbc:postgresql://%s:%s/%s",
+                env.getRequiredProperty("postgresql.dataSource.serverName"),
+                env.getRequiredProperty("postgresql.dataSource.portNumber"),
+                env.getRequiredProperty("postgresql.dataSource.databaseName")));
+        dataSource.setUsername(env.getRequiredProperty("postgresql.dataSource.user"));
+        dataSource.setPassword(env.getRequiredProperty("postgresql.dataSource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource());
+        liquibase.setChangeLog("classpath:db-master-changelog.xml");
+        return liquibase;
+    }
+}
