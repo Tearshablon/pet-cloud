3a9be92f2369f689b87fe68b559a6dfde25e01fc
dshablov <shablov.d@gmail.com>
2022-04-20 22:05:47 +0300

    fix

--- eureka/src/main/java/com/template/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/com/template/eureka/EurekaServerApplication.java
@@ -3,7 +3,9 @@ package com.template.eureka;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
     public static void main(String[] args) {
