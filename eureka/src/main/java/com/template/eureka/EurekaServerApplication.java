e84d49293f6d6f8845cf1216dbfa2a9c54e54d5d
dshablov <shablov.d@gmail.com>
2022-04-20 21:35:50 +0300

    fix

--- eureka/src/main/java/com/template/eureka/EurekaServerApplication.java
+++ eureka/src/main/java/com/template/eureka/EurekaServerApplication.java
@@ -1,14 +1,14 @@
-package com.template.registry;
+package com.template.eureka;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistryApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(RegistryApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+            SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+            app.setBannerMode(Banner.Mode.OFF);
+            app.run(args);
     }
 }
