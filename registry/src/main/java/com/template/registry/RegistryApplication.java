3dfacd864f62cc84376909e93fd81bc21045420a
dshablov <shablov.d@gmail.com>
2022-04-20 19:55:59 +0300

    add eureka

--- registry/src/main/java/com/template/registry/RegistryApplication.java
+++ registry/src/main/java/com/template/registry/RegistryApplication.java
@@ -3,7 +3,9 @@ package com.template.registry;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RegistryApplication {
     public static void main(String[] args) {
